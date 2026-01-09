f2d3936c7dff17045078d884a3e31d637b733232
Alison Bennett <Alison.Bennett@environmentalstandards.scot>
2026-01-09 16:38:20 +0000

    .

---
+++ scripts/2026_week1_bring-your-own-data_20260901.R
@@ -0,0 +1,11 @@
+####  ~~~~~~~~~~~~~~~~~~~~~~~~  Tidy Tuesday ~~~~~~~~~~~~~~~~~~~~~~~~~~~   ####
+##### 2026: week 1 
+
+# 09/01/2026
+# prompt: "Bring your own data"
+
+
+library(tidyverse)
+library(janitor)
+
+
